ce3a450e54cf74e6017f1f82ec9ab6051db546ca
sharestudy01 <96980884+sharestudy01@users.noreply.github.com>
2022-05-11 16:49:31 +0530

    Update and rename AddonsV-1.2.js to AddonsV-6.2.js

--- cdn/Javascripts/AddonsV-6.2.js
+++ cdn/Javascripts/AddonsV-6.2.js
@@ -68,8 +68,79 @@ for (
   );
 
 
-<!--[ AnitAd ]-->
-var lazyAnti=!1;var antiAdBlock=document.querySelector("#antiAdBlock");window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyAnti||0!=document.body.scrollTop&&!1===lazyAnti)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";e.onerror=function(){if(antiAdBlock!=null){antiAdBlock.classList.remove("hidden");window.lazyAnti=!0}};var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyAnti=!0)},!0);
+<!--[ lazy AnitAd ]-->
+var lazyAnti = !1;
+var antiAdBlock = document.querySelector("#antiAdBlock");
+window.addEventListener(
+  "scroll",
+  function () {
+    ((0 != document.documentElement.scrollTop && !1 === lazyAnti) ||
+      (0 != document.body.scrollTop && !1 === lazyAnti)) &&
+      (!(function () {
+        var e = document.createElement("script");
+        (e.type = "text/javascript"),
+          (e.async = !0),
+          (e.src =
+            "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js");
+        e.onerror = function () {
+          if (antiAdBlock != null) {
+            antiAdBlock.classList.remove("hidden");
+            window.lazyAnti = !0;
+          }
+        };
+        var a = document.getElementsByTagName("script")[0];
+        a.parentNode.insertBefore(e, a);
+      })(),
+      (lazyAnti = !0));
+  },
+  !0
+);
 
+     <!-- Google Translate -->
+var lazyts = !1;
+var gTrans = document.getElementById("gTrans");
+window.addEventListener(
+  "scroll",
+  function () {
+    ((0 != document.documentElement.scrollTop && !1 === lazyts) ||
+      (0 != document.body.scrollTop && !1 === lazyts)) &&
+      (!(function () {
+        var e = document.createElement("script");
+        (e.type = "text/javascript"),
+          (e.async = !0),
+          (e.src =
+            "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit");
+        var a = document.getElementsByTagName("script")[0];
+        a.parentNode.insertBefore(e, a);
+        if (gTrans != null) {
+          gTrans.classList.remove("gtHide");
+        }
+      })(),
+      (lazyts = !0));
+  },
+  !0
+);
 
+     <!-- Back to Top --> 
+var backTop = document.getElementById("backTop");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 150 ||
+    document.documentElement.scrollTop > 150
+  ) {
+    backTop.style.display = "block";
+    backTop.style.bottom = "20px";
+  } else {
+    backTop.style.display = "none";
+  }
+  if (
+    window.innerHeight + window.pageYOffset >=
+    document.body.offsetHeight - 100
+  ) {
+    backTop.style.bottom = "150px";
+  }
+}
 
